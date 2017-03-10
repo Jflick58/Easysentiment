717a1aa6dc9d05caec59ebe1a59b842a65c1409f
Justin Flick <Justinjayflick@gmail.com>
2017-03-10 14:10:56 -0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,16 +1,7 @@
-##Twitter Scraper and Sentiment Analysis
-##Justin Flick 
-##Copyright 2017 Licensed under MIT License 
-##Script for install libraries needed to run this program
+from setuptools import setup
 
-import subprocess
-
-def install(name):
-    subprocess.call(['pip', 'install', 'textblob'])
-    
-def install(name):
-    subprocess.call(['pip', 'install', 'twitterscraper'])
-    
-def install(name):
-    subprocess.call(['pip', 'install', 'easygui'])
-    
+setup(
+	name='easysentiment',
+	version= '1.1',
+	scripts= ['scraper_and_analysis.py','scraper.py','sentiment_analysis.py']
+)
